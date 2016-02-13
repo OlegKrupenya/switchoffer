479cee9ae96879f8350c5c3ce3776461f2ba94b4
Oleh Krupenia <oleh.krupenia@globallogic.com>
2016-02-13 22:57:03 +0200

    alignment fix

--- src/main/java/com/testdev/main/Main.java
+++ src/main/java/com/testdev/main/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Controller controller = SpringFXMLLoader.load("/sample.fxml");
-        Scene scene = new Scene((Parent) controller.getView());
+        Scene scene = new Scene((Parent) controller.getView(), 300, 260);
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Shut down the computer");
